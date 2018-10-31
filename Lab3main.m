f110c40580507b2958f06c34f07ceb4e02292650
William Watkins <william.watkins@colorado.edu>
2018-10-31 14:11:31 -0600

    Update Lab3main.m

--- Lab3main.m
+++ Lab3main.m
@@ -9,6 +9,10 @@ ATemp = Data(:,2);
 Voltage = Data(:,7);
 
 AvPressure = mean(APressure);
+
+%Manometer Diff heights
+manometer = [0; .4; 1.4; 2.8; 4.9];
+uncmano = .05;
 %{
 Airspeed = AirSpeed();
 
