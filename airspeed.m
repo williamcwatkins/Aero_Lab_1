c994f193c184630d281392c6e455c121c478e6e1
William Watkins <william.watkins@colorado.edu>
2018-10-31 13:59:11 -0600

    Update airspeed.m

--- airspeed.m
+++ airspeed.m
@@ -1,11 +1,12 @@
-function [speed,speed_av]= airspeed(Data)
-    APressure= Data(:,1)/1000;
-    %Convert to kilo pascal
-    R=.287; %kg/kJ*k
-    Temp=Data(:,2);
-    rho=APressure./(R*Temp);
-    gage_pressure=Data(:,3);
-    %Keep in pascals
-    speed=sqrt(2.*gage_pressure)./(rho);
-    speed_av=mean(speed);
+function [speed,speed_av]= AirSpeed(Data)
+    %AirSpeed calculates the airspeed from the velocity voltage data
+    APressure = Data(:,1)/1000;
+    %Convert atmospheric pressure to kilo pascal
+    R = 0.287; %kg/kJ*k
+    Temp = Data(:,2);
+    rho = APressure./(R*Temp);
+    gage_pressure = Data(:,3);
+    %Keep the differential pressure in pascals
+    speed = sqrt(2.*gage_pressure)./(rho);
+    speed_av = mean(speed);
 end
