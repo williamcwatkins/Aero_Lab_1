3b85be62fa4e9926e0e682963b9e0b00053a88b9
erst0636 <44448517+erst0636@users.noreply.github.com>
2018-11-07 15:57:54 -0700

    Add files via upload

--- Calc_Boundary_Layer.m
+++ Calc_Boundary_Layer.m
@@ -1,5 +1,5 @@
 %% Calulate Boundry Layer
-clear;clc
+clear;clc;close all
 %Port Number 3
 %height recorded 0, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, and 
 %Center
@@ -15,25 +15,45 @@ input = 'Aero_Lab_1-master\BoundaryLayer_S013_G03.csv';
 data  = load(input);
 
 %Probe is actually .4mm off the ground
-if length(data) == 6000
-    %loop through the 12 data points
+    %INTIALIZE LOOP ITERATORS
     y_val = 0;
-for loop1 = 1:13
-    %Loop through the data to get the 
-    for loop2 = 1:length(data)
-        level_data = zeros(500,7);
-        if (data(loop2,6) > -0.1+y_val) && (data(loop2,6) < 0.1+y_val)
-            level_data(loop2,:)= data(loop2,:);
-        else
-            break;
-        end
+    k = 1;
+    k_stop = 500:500:length(data);
+for loop1 = 1:length(k_stop)
+    %Loop through the data to get the level data of the current y_val
+    %height
+    %Preallocating for speed
+    level_data = zeros(500,7);
+    i = 1;
+    while k <= k_stop(loop1)
+        level_data(i,:)= data(k,:);
+        k = k+1;
+        i = i+1;
     end
-    
     %Now use the data to calculate the air velocity at this height and save
     %it in matrix called wind speed pressure diff over the density times 2
+    [ASpeed,ASpeedAv]= AirSpeed_Boundary(level_data);
+    y_val=mean(level_data(:,6))+.4;
+   file_data(loop1,:) = [y_val,ASpeedAv];
+    
 end
+free_stream_v = file_data(end,2);
+boundary_v = free_stream_v*.95;
+ELD_height = file_data(1:end-1,1);
+ELD_v = file_data(1:end-1,2);
+hold on
+plot(ELD_v,ELD_height)
+p=polyfit(ELD_v,ELD_height,2);
 
-else 
+line([boundary_v,boundary_v],[0,10])
+x = 16:.1:28;
+f = p(1).*x.^2+p(2).*x+p(3);
+plot(x,f)
+
+for h=1:length(f)-1
+   if x(h) < boundary_v && x(h+1)> boundary_v
+       height=h;
+   end
 end
-            error('Data in %f mm range not found for file %s',y_val,input)
+height=f(height);
 
