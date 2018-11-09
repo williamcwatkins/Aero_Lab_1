561f175839811c784ebd1fb3fe5e54e658ae8352
PaulAndler <44448538+PaulAndler@users.noreply.github.com>
2018-11-09 08:25:13 -0700

    Add files via upload

--- readin.m
+++ readin.m
@@ -1,7 +1,7 @@
 
 filenameB = {'BoundaryLayer_S013_G01.csv','BoundaryLayer_S013_G03.csv'};
 k = 1;
-for i = 1:2
+for i = 1:length(filenameB)
     BData{k} = csvread(filenameB{i},1);
     k = k+1;
 end
@@ -9,7 +9,7 @@ end
 
 filenameV = {'VelocityVoltage_S013_G03.csv'};
 k = 1;
-for i = 1:2
+for i = 1:length(filenameV)
     vData{k} = csvread(filenameV{i},1);
     k = k+1;
 end
